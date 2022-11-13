c63a966df328da848ca0404d685bd1b9a432db6a
Tamzid Israk <tamzidisrak12@gmail.com>
2022-11-14 02:48:43 +0600

    Face Detection migrate to androidx,create user interface ,bottom sheet bottomSheet RecyclerView, init camerView,bottomSheet,toggleBtn

--- app/src/main/java/com/example/facedetector/MainActivity.java
+++ app/src/main/java/com/example/facedetector/MainActivity.java
@@ -1,15 +1,56 @@
 package com.example.facedetector;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.facedetector.model.FaceDetectionModel;
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
+import com.otaliastudios.cameraview.CameraView;
+import com.otaliastudios.cameraview.controls.Facing;
+import com.otaliastudios.cameraview.frame.Frame;
+import com.otaliastudios.cameraview.frame.FrameProcessor;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements FrameProcessor {
+    private Facing camerFacing = Facing.FRONT;
+    private ImageView imgView;
+    private CameraView faceDetectionCameraView;
+    private RecyclerView bottomSheetRecyclerView;
+    private BottomSheetBehavior bottomSheetBehavior;
+    private ArrayList<FaceDetectionModel> faceDetectionModels;
+    private Button toggleBtn;
+    private FrameLayout bottomSheetBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
+
+        faceDetectionModels = new ArrayList<>();
+        bottomSheetBehavior = BottomSheetBehavior.from(findViewById(R.id.bottom_sheet));
+        imgView =   findViewById(R.id.face_detection_image_view);
+        faceDetectionCameraView = findViewById(R.id.face_detection_camera_view);
+        toggleBtn = findViewById(R.id.face_detection_camera_toggle_button);
+        bottomSheetBtn = findViewById(R.id.bottom_sheet_button);
+        bottomSheetRecyclerView = findViewById(R.id.bottom_sheet_recycler_view);
+
+        faceDetectionCameraView.setFacing(camerFacing);
+        faceDetectionCameraView.setLifecycleOwner(MainActivity.this);
+        faceDetectionCameraView.addFrameProcessor(MainActivity.this);
+
+    }
+
+    @Override
+    public void process(@NonNull Frame frame) {
+
     }
 }
